5e11fe960f8b754d7e9934f2fb1bb5d6d9cf0a4d
Eugene Liu <eugene.liu@intel.com>
2023-07-10 21:05:26 +0100

    update

---
+++ custom_configs/_base_/datasets/hubmap_strategy5_cls1.py
@@ -0,0 +1,57 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '/home/yuchunli/_DATASET/HuBMAP-vasculature-coco-cls_1-s5/'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+classes = ['blood_vessel']
+
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=False),
+    dict(type='RandomFlip', flip_ratio=0.5, direction=['horizontal', 'vertical']),
+    dict(type='Pad', size_divisor=32),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks'])
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=False),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_train.json',
+        img_prefix=data_root + 'images/train/',
+        pipeline=train_pipeline,
+        classes=classes),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val.json',
+        img_prefix=data_root + 'images/val/',
+        pipeline=test_pipeline,
+        classes=classes),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/instances_val.json',
+        img_prefix=data_root + 'images/val/',
+        pipeline=test_pipeline,
+        classes=classes))
---
+++ custom_configs/hubmap/solov2_x101_dcn_fpn_hubmap_s5_cls1.py
@@ -0,0 +1,34 @@
+_base_ = [
+    './solov2_x101_dcn_fpn.py',
+    '../_base_/datasets/hubmap_strategy5_cls1.py',
+]
+
+model = dict(mask_head=dict(num_classes=1))
+
+data = dict(samples_per_gpu=4)
+
+runner = dict(type='EpochBasedRunnerWithCancel', max_epochs=100)
+
+# optimizer_config = dict(_delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+
+lr_config = dict(
+    _delete_=True,
+    policy='ReduceLROnPlateau',
+    metric='mAP',
+    patience=5,
+    iteration_patience=0,
+    interval=1,
+    min_lr=1e-06,
+    warmup='linear',
+    warmup_iters=200,
+    warmup_ratio=0.3333333333333333)
+
+custom_hooks = [
+    dict(
+        type='EarlyStoppingHook',
+        patience=10,
+        iteration_patience=0,
+        metric='segm_mAP',
+        interval=1,
+        priority=75)
+]
