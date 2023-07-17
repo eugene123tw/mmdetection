cdc6d67413458512db85c48fec53ed8340f9a3b1
Eugene Liu <eugene.liu@intel.com>
2023-07-17 14:12:19 +0100

    udpate

--- custom_configs/_base_/datasets/hubmap_1344x1344_s5_cls1.py
+++ custom_configs/_base_/datasets/hubmap_1344x1344_s5_cls1.py
@@ -6,62 +6,14 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 classes = ['blood_vessel']
 
-albu_train_transforms = [
-    dict(
-        type='RandomBrightnessContrast',
-        brightness_limit=[0.1, 0.3],
-        contrast_limit=[0.1, 0.3],
-        p=0.2),
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(
-                type='RGBShift',
-                r_shift_limit=10,
-                g_shift_limit=10,
-                b_shift_limit=10,
-                p=1.0),
-            dict(
-                type='HueSaturationValue',
-                hue_shift_limit=20,
-                sat_shift_limit=30,
-                val_shift_limit=20,
-                p=1.0)
-        ],
-        p=0.2),
-    dict(type='JpegCompression', quality_lower=85, quality_upper=95, p=0.2),
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(type='Blur', blur_limit=3, p=1.0),
-            dict(type='MedianBlur', blur_limit=3, p=1.0)
-        ],
-        p=0.2),
-    dict(type='RandomRotate90', p=0.5)
-]
-
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
-    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=False),
-    dict(type='RandomFlip', direction='vertical', flip_ratio=0.5),
-    dict(type='RandomFlip', direction='horizontal', flip_ratio=0.5),
+    dict(type='Resize', img_scale=(1344, 1344), keep_ratio=False),
     dict(
-        type='Albu',
-        transforms=albu_train_transforms,
-        bbox_params=dict(
-            type='BboxParams',
-            format='pascal_voc',
-            label_fields=['gt_labels'],
-            min_visibility=0.0,
-            filter_lost_elements=True),
-        keymap={
-            'img': 'image',
-            'gt_masks': 'masks',
-            'gt_bboxes': 'bboxes'
-        },
-        update_pad_shape=False,
-        skip_img_without_anno=True),
+        type='RandomFlip',
+        flip_ratio=0.5,
+        direction=['horizontal', 'vertical']),
     dict(type='Pad', size_divisor=32),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='DefaultFormatBundle'),
@@ -72,7 +24,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1024, 1024),
+        img_scale=(1344, 1344),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=False),
